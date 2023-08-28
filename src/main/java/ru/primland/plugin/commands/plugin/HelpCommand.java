62e2408c9f1db06b8f8fbc2c8a5d5dcc8d5be564
stngularity <stngularity@gmail.com>
2023-08-28 21:38:42 +0300

    Переписал команду /pp help

--- src/main/java/ru/primland/plugin/commands/plugin/HelpCommand.java
+++ src/main/java/ru/primland/plugin/commands/plugin/HelpCommand.java
@@ -1,159 +1,103 @@
 package ru.primland.plugin.commands.plugin;
 
+import io.github.stngularity.epsilon.engine.placeholders.IPlaceholder;
 import io.github.stngularity.epsilon.engine.placeholders.Placeholder;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.jetbrains.annotations.NotNull;
-import ru.primland.plugin.Config;
+import org.jetbrains.annotations.Nullable;
 import ru.primland.plugin.PrimPlugin;
-import ru.primland.plugin.modules.IPluginModule;
+import ru.primland.plugin.commands.manager.*;
+import ru.primland.plugin.commands.manager.argument.type.IntegerArgument;
 import ru.primland.plugin.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class HelpCommand implements IPluginCommand {
+@CommandInfo(
+        name="help",
+        description="Помощь по командам плагина",
+        parent="primplugin"
+)
+public class HelpCommand extends Command {
+    private static int commandsAtPage;
+
     /**
-     * Выполняет команду плагина
-     * @param sender Отправитель команды
-     * @param args Аргументы команды
+     * Загрузить данные команды
+     *
+     * @param plugin Экземпляр плагина
      */
     @Override
-    public void execute(@NotNull CommandSender sender, @NotNull List<String> args) {
-        PrimPlugin plugin = PrimPlugin.getInstance();
-        Config config = Config.load("commands/plugin_help.yml");
-        StringBuilder message = new StringBuilder();
-
-        if(args.size() == 0) {
-            message.append(String.join("\n", config.getStringList("headers.modules"))).append("\n");
-            message.append(Utils.parse(config.getString("content.module"), new Placeholder("name", "primary"),
-                    new Placeholder("description", "Основные команды плагина"),
-                    new Placeholder("commands", plugin.getPrimaryCommand().getCommands().size()- plugin.getManager()
-                            .getModuleSubCommands().size())));
-
-            plugin.getManager().getModules().forEach(module -> {
-                if(config.getBoolean("ignoreModulesWithoutCommands", true) && plugin.getManager().getModuleSubCommands(module.getName()).size() == 0)
-                    return;
-
-                message.append("\n");
-                message.append(Utils.parse(config.getString("content.module"), new Placeholder("name", module.getName()),
-                        new Placeholder("description", module.getDescription()),
-                        new Placeholder("commands", plugin.getManager().getModuleSubCommands(module
-                                .getName()).size())));
-            });
-
-            message.append(String.join("\n", config.getStringList("footer")));
-            sender.sendMessage(Utils.parse(message.toString()));
-            return;
-        }
-
-        String name = args.get(0);
-
-        List<String> header = new ArrayList<>();
-        config.getStringList("headers.commands").forEach(line -> header.add(Utils.parse(line,
-                new Placeholder("module", name))));
-
-        message.append(String.join("\n", header)).append("\n");
-        if(name.equals("primary")) {
-            message.append(commandsToString(sender, plugin.getPrimaryCommand().getCommands()));
-            message.append(String.join("\n", config.getStringList("footer")));
-            sender.sendMessage(Utils.parse(message.toString()));
-            return;
-        }
-
-        IPluginModule module = plugin.getManager().getModule(name);
-        if(module == null) {
-            PrimPlugin.send(sender, Utils.parse(config.getString("errors.moduleNotFound"),
-                    new Placeholder("module", name)));
-
-            return;
-        }
-
-        if(plugin.getManager().getModuleSubCommands(module.getName()).size() == 0) {
-            PrimPlugin.send(sender, Utils.parse(config.getString("errors.moduleHasNotCommands"),
-                    new Placeholder("module", name)));
-
-            return;
-        }
-
-        message.append(commandsToString(sender, plugin.getManager().getModuleSubCommands(module.getName())));
-        message.append(String.join("\n", config.getStringList("footer")));
-        sender.sendMessage(Utils.parse(message.toString()));
+    public void load(PrimPlugin plugin) {
+        addArgument(new IntegerArgument("page", "номер страницы", false));
+        commandsAtPage = PrimPlugin.i18n.getInteger("commandsAtPage", 6);
     }
 
     /**
-     * Конвертирует список команд в строку
-     * @param sender Отправитель команды
-     * @param commands Список команд
-     * @return Строка с командами
+     * Отгрузить данные команды
+     *
+     * @param plugin Экземпляр плагина
      */
-    private @NotNull String commandsToString(CommandSender sender, @NotNull List<IPluginCommand> commands) {
-        Config config = Config.load("commands/plugin_help.yml");
+    @Override
+    public void unload(PrimPlugin plugin) {
+        commandsAtPage = 0;
+    }
 
-        StringBuilder output = new StringBuilder();
-        commands.forEach(cmd -> {
-            if(PluginPrimaryCommand.hasNotPermissions(sender, cmd.getRequiredPermissions()))
+    /**
+     * Выполнить команду с указанными данными
+     *
+     * @param ctx Контекст команды
+     * @return Сообщение для отправителя команды
+     */
+    @Override
+    public @Nullable String execute(@NotNull CommandContext ctx) {
+        @Nullable Integer page = ctx.get("page");
+        return getResult(ctx, page == null ? 1 : page);
+    }
+
+    /**
+     * Получить результат для команды /pp help
+     *
+     * @param ctx  Контекст команды
+     * @param page Номер страницы с командами
+     * @return Готовый результат
+     */
+    public static @NotNull String getResult(CommandContext ctx, int page) {
+        // TODO: Доделать EpsilonEngine и закинуть сюда кликабельные компоненты
+        Map<String, CachedCommand> commands = CommandManager.cache;
+
+        List<IPlaceholder> placeholders = new ArrayList<>();
+        placeholders.add(new Placeholder("page", page));
+        placeholders.add(new Placeholder("pages", commands.size() / commandsAtPage));
+
+        StringBuilder output = new StringBuilder(Utils.parse(String.join("\n",
+                PrimPlugin.i18n.getStringList("globalHelp.header"))));
+
+        AtomicInteger index = new AtomicInteger(0);
+        commands.forEach((name, command) -> {
+            if(index.get() >= commandsAtPage)
                 return;
 
-            output.append(Utils.parse(config.getString("content.command"),
-                    new Placeholder("name", cmd.getName()),
-                    new Placeholder("usage", cmd.getUsage().length() > 0 ? " " + cmd.getUsage() : ""),
-                    new Placeholder("description", cmd.getDescription())));
+            CommandInfo info = command.getInfo();
+            if(!ctx.sender.hasPermission(info.permission()))
+                return;
+
+            if(info.playersOnly() && ctx.sender instanceof ConsoleCommandSender)
+                return;
+
+            List<IPlaceholder> cmdPlaceholders = new ArrayList<>(placeholders);
+            cmdPlaceholders.add(new Placeholder("name", (info.parent().isEmpty() ? "" : info.parent() + " ") + name));
+            cmdPlaceholders.add(new Placeholder("description", info.description()));
+            cmdPlaceholders.add(new Placeholder("aliases", String.join(", ", info.aliases())));
+            cmdPlaceholders.add(new Placeholder("permission", info.permission()));
+            cmdPlaceholders.add(new Placeholder("usage", CommandManager.getUsage(command.getCommand())));
+
+            output.append(Utils.parse(PrimPlugin.i18n.getString("globalHelp.content"), cmdPlaceholders));
+            index.getAndIncrement();
         });
 
-        return output.toString();
-    }
-
-    /**
-     * "Подсказывает" игроку значение аргумента
-     * @param sender Отправитель команды
-     * @param args Аргументы команды
-     * @return Список с подсказками для аргумента
-     */
-    @Override
-    public List<String> tabComplete(@NotNull CommandSender sender, @NotNull String @NotNull [] args) {
-        if(Math.max(args.length-1, 0) == 1) {
-            List<String> modules = PrimPlugin.getInstance().getManager().getModulesNames();
-            modules.add(0, "primary");
-            return modules;
-        }
-
-        return null;
-    }
-
-    /**
-     * Получает и возвращает название этой команды
-     * @return Название команды
-     */
-    @Override
-    public String getName() {
-        return "help";
-    }
-
-    /**
-     * Получает и возвращает описание этой команды
-     * @return Описание команды
-     */
-    @Override
-    public String getDescription() {
-        return "Получение помощи по командам";
-    }
-
-    /**
-     * Получает и возвращает разрешения, требуемые для выполнения
-     * этой команды
-     * @return Требуемые разрешения
-     */
-    @Override
-    public List<String> getRequiredPermissions() {
-        return List.of("primplugin.commands.primary");
-    }
-
-    /**
-     * Получает и возвращает инструкцию по использованию этой команды
-     * @return Инструкция по использованию команды
-     */
-    @Override
-    public String getUsage() {
-        return "[модуль]";
+        output.append(Utils.parse(String.join("\n", PrimPlugin.i18n.getStringList("globalHelp.footer"))));
+        return Utils.parse(output.toString(), placeholders);
     }
 }
