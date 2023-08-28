65bbf20a0f2a17f3d7a99585ec05888c2b49b66b
stngularity <stngularity@gmail.com>
2023-08-28 20:59:27 +0300

    Переписал команду /help

--- src/main/java/ru/primland/plugin/commands/HelpCommand.java
+++ src/main/java/ru/primland/plugin/commands/HelpCommand.java
@@ -2,16 +2,16 @@ package ru.primland.plugin.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.primland.plugin.Config;
 import ru.primland.plugin.PrimPlugin;
+import ru.primland.plugin.commands.manager.Command;
+import ru.primland.plugin.commands.manager.CommandContext;
+import ru.primland.plugin.commands.manager.CommandInfo;
 import ru.primland.plugin.utils.CustomMenu;
 import ru.primland.plugin.utils.NBTUtils;
 import ru.primland.plugin.utils.Utils;
@@ -22,22 +22,45 @@ import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MCHelpCommand implements CommandExecutor {
+@CommandInfo(
+        name="help",
+        description="Справка по механикам сервера",
+        aliases={"h"},
+        playersOnly=true
+)
+public class HelpCommand extends Command {
     private final static Pattern actionPattern = Pattern.compile("\\[([a-zA-Z0-9_]+)] *(.*)?");
 
-    private Config config;
+    public Config config;
 
-    public MCHelpCommand(Config config) {
-        this.config = config;
+    /**
+     * Загрузить данные команды
+     *
+     * @param plugin Экземпляр плагина
+     */
+    @Override
+    public void load(PrimPlugin plugin) {
+        config = Config.load("commands/minecraft_help.yml");
     }
 
+    /**
+     * Отгрузить данные команды
+     *
+     * @param plugin Экземпляр плагина
+     */
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
-        if(sender instanceof ConsoleCommandSender) {
-            PrimPlugin.send(PrimPlugin.getInstance().getI18n().getString("playersOnly"));
-            return true;
-        }
+    public void unload(PrimPlugin plugin) {
+        config = null;
+    }
 
+    /**
+     * Выполнить команду с указанными данными
+     *
+     * @param ctx Контекст команды
+     * @return Сообщение для отправителя команды
+     */
+    @Override
+    public @Nullable String execute(@NotNull CommandContext ctx) {
         int size = config.getInteger("size", 9);
         CustomMenu menu = new CustomMenu(config.getString("title"), size);
 
@@ -54,12 +77,10 @@ public class MCHelpCommand implements CommandExecutor {
                 itemStack = NBTUtils.addTexture(itemStack, item.get("texture").toString());
 
             ItemMeta meta = itemStack.getItemMeta();
-            if(meta == null) return;
-
-            if(item.containsKey("displayName"))
+            if(meta != null && item.containsKey("displayName"))
                 meta.setDisplayName(Utils.parse(item.get("displayName").toString()));
 
-            if(item.containsKey("lore")) {
+            if(meta != null && item.containsKey("lore")) {
                 List<String> lore = new ArrayList<>();
                 Utils.convertObjectToList(item.get("lore")).forEach(line ->
                         lore.add(Utils.parse(line.toString())));
@@ -83,24 +104,24 @@ public class MCHelpCommand implements CommandExecutor {
                     if(actionType.equals("SOUND"))
                         Utils.playSound(player, actionData);
 
-                    if(actionType.equals("MESSAGE") && actionData.length() == 0)
+                    if(actionType.equals("MESSAGE") && actionData.isEmpty())
                         PrimPlugin.send("&cУкажите сообщение! (minecraft_help.yml)");
 
-                    if(actionType.equals("MESSAGE") && actionData.length() > 0)
+                    if(actionType.equals("MESSAGE") && !actionData.isEmpty())
                         player.sendMessage(Utils.parse(actionData));
 
-                    if(actionType.equals("PLAYER") && actionData.length() == 0)
+                    if(actionType.equals("PLAYER") && actionData.isEmpty())
                         PrimPlugin.send("&cУкажите команду (без /)! (minecraft_help.yml)");
 
-                    if(actionType.equals("PLAYER") && actionData.length() > 0)
+                    if(actionType.equals("PLAYER") && !actionData.isEmpty())
                         player.performCommand(actionData);
 
-                    if(actionType.equals("CONSOLE") && actionData.length() == 0)
+                    if(actionType.equals("CONSOLE") && actionData.isEmpty())
                         PrimPlugin.send("&cУкажите команду (без /)! (minecraft_help.yml)");
 
-                    if(actionType.equals("CONSOLE") && actionData.length() > 0)
+                    if(actionType.equals("CONSOLE") && !actionData.isEmpty())
                         Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), actionData);
-                    
+
                     if(actionType.equals("CLOSE"))
                         cmenu.close(true);
                 }));
@@ -112,11 +133,7 @@ public class MCHelpCommand implements CommandExecutor {
             menu.setItem((int) item.get("slot"), itemStack, null);
         });
 
-        menu.open((Player) sender);
-        return true;
-    }
-
-    public void updateConfig(Config config) {
-        this.config = config;
+        ctx.open(menu);
+        return null;
     }
 }
